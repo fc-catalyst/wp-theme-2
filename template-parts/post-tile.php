267ad23c2713d4427862298e7217d5e9d42d033b
VVolkov833 <vadim.volkov.dev@gmail.com>
2023-06-06 14:40:00 +0200

    blog layout & styling ready

--- template-parts/post-tile.php
+++ template-parts/post-tile.php
@@ -1,27 +1,31 @@
 <?php
 
 $cat = get_the_category( get_the_ID() )[0];
-$cat = $cat ? '<a href="'.esc_url( get_category_link( $cat ) ).'" class="entry-category">'.esc_html( $cat->name ).'</a>' : '';
+$cat = $cat ? '<a href="'.esc_url( get_category_link( $cat ) ).'">'.esc_html( $cat->name ).'</a>' : '';
 
 ?>
-<article class="post-<?php the_ID() ?> <?php echo get_post_type() ?> type-<?php echo get_post_type() ?> status-<?php echo get_post_status() ?> entry" itemscope="" itemtype="https://schema.org/Article">
-    <header class="entry-header">
-        <?php echo $cat ?>
-        <div class="entry-photo">
-            <?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'medium' ); } ?>
-        </div>
-        <h2 class="entry-title" itemprop="headline">
-            <a class="entry-title-link" href="<?php the_permalink() ?>"><?php the_title() ?></a>
+<article class="post-<?php the_ID() ?> <?php echo get_post_type() ?> type-<?php echo get_post_type() ?> status-<?php echo get_post_status() ?> post-preview post" itemscope="" itemtype="https://schema.org/Article">
+    <a href="<?php the_permalink() ?>" class="post-thumbnail featuredimage">
+        <?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'medium' ); } ?>
+    </a>
+    <header class="post-header">
+        <h2 class="post-title" itemprop="headline">
+            <a class="post-title-link" href="<?php the_permalink() ?>"><?php the_title() ?></a>
         </h2>
-    </header>
-    <div class="entry-details">
-        <div class="entry-date" itemprop="datePublished" content="<?php the_date('Y-m-d') ?>">
+        <!--
+        <time class="post-meta post-date" itemprop="datePublished" content="<?php the_date('Y-m-d') ?>" datetime="<?php the_date('Y-m-d') ?>">
             <?php echo get_the_date() ?>
-        </div>
-        <div class="entry-excerpt">
-            <?php the_excerpt() ?>
-        </div>
-        <a href="<?php the_permalink() ?>" class="entry-read"><?php _e( 'Read more' ) ?></a>
+        </time>
+        -->
+        <?php if ( $cat ) { ?>
+            <div class="post-meta post-category">
+                <?php echo $cat ?>
+            </div>
+        <?php } ?>
+    </header>
+    <div class="post-excerpt">
+        <?php the_excerpt() ?>
     </div>
+    <a href="<?php the_permalink() ?>" title="<?php echo esc_attr( get_the_title() ) ?>" class="post-readmore post-button"><?php _e( 'Read more' ) ?></a>
 </article>
 <?php
