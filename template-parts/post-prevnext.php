636d0959530ddc59430a811713291a48ce9355c1
VVolkov833 <vadim.volkov.dev@gmail.com>
2023-06-07 20:57:46 +0200

    pagination ready

--- template-parts/post-prevnext.php
+++ template-parts/post-prevnext.php
@@ -1,12 +1,6 @@
-<nav class="wp-block-columns nav-prev-next">
-    <div class="wp-block-column">
-		<p>
-            <?php next_post_link( '%link', __( 'Previous Post', 'fct' ) ) ?>
-        </p>
-    </div>
-    <div class="wp-block-column">
-		<p style="text-align:right;">
-            <?php previous_post_link( '%link', __( 'Next Post', 'fct' ) ) ?>
-        </p>
-	</div>
+<nav class="pagination">
+  <ul>
+      <?php next_post_link( '<li>%link</li>', __( 'Previous Post', 'fct' ) ) ?>
+      <?php previous_post_link( '<li>%link</li>', __( 'Next Post', 'fct' ) ) ?>
+  </ul>
 </nav>
